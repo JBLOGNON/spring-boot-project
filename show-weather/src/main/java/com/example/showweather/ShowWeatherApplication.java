3564901c8c4ac97b3b9db5b4089b9d656a77dde9
JBLOGNON <43005236+JBLOGNON@users.noreply.github.com>
2022-02-18 16:51:09 +0100

    Eureka + Load Balance

--- show-weather/src/main/java/com/example/showweather/ShowWeatherApplication.java
+++ show-weather/src/main/java/com/example/showweather/ShowWeatherApplication.java
@@ -1,13 +1,18 @@
 package com.example.showweather;
 
+import com.example.showweather.configuration.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableHystrixDashboard
 @EnableCircuitBreaker
+@RibbonClient(name = "server", configuration = RibbonConfiguration.class)
+@EnableEurekaClient
 public class ShowWeatherApplication {
 
     public static void main(String[] args) {
--- show-weather/src/main/java/com/example/showweather/delegate/WeatherServiceDelegate.java
+++ show-weather/src/main/java/com/example/showweather/delegate/WeatherServiceDelegate.java
@@ -2,6 +2,7 @@ package com.example.showweather.delegate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -22,18 +23,19 @@ public class WeatherServiceDelegate {
 
     @HystrixCommand(fallbackMethod = "getVilleFallback")
     public String getVilleByName(String name) {
-        String response = restTemplate.exchange("http://localhost:8080/getVilleByName/{name}",
+        String response = restTemplate.exchange("http://weather/getVilleByName/{name}",
                 HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, name).getBody();
         return "Informations de " + name + " : [" + response + "].";
     }
 
     @HystrixCommand(fallbackMethod = "getVilleFallback")
     public String getVilleByZipCode(String zip) {
-        String response = restTemplate.exchange("http://localhost:8080/getVilleByZipCode/{zip}",
+        String response = restTemplate.exchange("http://weather/getVilleByZipCode/{zip}",
                 HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, zip).getBody();
         return "Informations de " + zip + " : [" + response + "].";
     }
 
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
